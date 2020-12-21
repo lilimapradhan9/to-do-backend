cfeeabc32031c2683abfa64e3dc2914bad7ae1da
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2020-12-21 21:53:23 +0530

    Lilima | Adding docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-service'),
-    TodoModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), TodoModule],
 })
 export class AppModule {}
